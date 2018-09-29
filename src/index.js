81282d1f513411b98c03eb3e06943f089389cbff
Adrian <38096991+Boobaley@users.noreply.github.com>
2018-09-29 21:44:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if (str.length % 2) {
+    return false;
+  }
+  const specialCharacters = new Set(['(', ')', '{', '}', '[', ']', '|']);
+
+  function createRegExpGroup(config) {
+    const leftChar = config[0];
+    const rightChar = config[1];
+
+    return (specialCharacters.has(leftChar) ? '\\' + leftChar : leftChar) +  (specialCharacters.has(rightChar) ? '\\' + rightChar : rightChar);
+  }
+  
+  let pairsToGo = str.length / 2;
+
+  const regExp = new RegExp(bracketsConfig.map(config => createRegExpGroup(config)).join('|'), 'g');
+
+  while(pairsToGo && str) {
+    str = str.replace(regExp, '');
+    pairsToGo -= 1;
+  }
+
+  return !str;
 }
